817f40b3d9024c5a9ed55ce69cea57dc50b95314
MandaliWala <141039684+MandaliWala@users.noreply.github.com>
2025-08-24 11:10:06 +0530

    try

--- plugins/auto_reply.py
+++ plugins/auto_reply.py
@@ -21,10 +21,9 @@ async def auto_reply(bot, message):
             logger.warning(f"Got FloodWait.\n\nWaiting for {e.value} seconds.")
             await asyncio.sleep(e.value + 2)
             logger.info("Floodwait ended")
-
-    try:
-        await asyncio.sleep(17)
-        await reply.delete()
-    except:
-        pass
+        try:
+            await asyncio.sleep(17)
+            await reply.delete()
+        except:
+            pass
             
