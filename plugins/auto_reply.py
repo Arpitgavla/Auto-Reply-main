ec0e70ff7910aa1f4096048b6194939051627269
MandaliWala <141039684+MandaliWala@users.noreply.github.com>
2025-08-24 12:00:17 +0530

    try again

--- plugins/auto_reply.py
+++ plugins/auto_reply.py
@@ -11,19 +11,16 @@ lock = asyncio.Lock()
 
 @Client.on_message(filters.chat(FROM_GRP) & ~filters.bot)
 async def auto_reply(bot, message):
-    async with lock:
-        try:
-            reply = await message.reply_text('''Υσυя мσνιє ιѕ ιη му ƀισ, ρℓєαѕє cнєcк му ρяσƒιℓє.
+    try:
+        reply = await message.reply_text('''Υσυя мσνιє ιѕ ιη му ƀισ, ρℓєαѕє cнєcк му ρяσƒιℓє.
 ''', reply_to_message_id=message.id)
-
-            await asyncio.sleep(2)
-        except FloodWait as e:
-            logger.warning(f"Got FloodWait.\n\nWaiting for {e.value} seconds.")
-            await asyncio.sleep(e.value + 2)
-            logger.info("Floodwait ended")
+        await asyncio.sleep(2)
         try:
             await asyncio.sleep(17)
             await reply.delete()
         except:
             pass
-            
+    except FloodWait as e:
+        logger.warning(f"Got FloodWait.\n\nWaiting for {e.value} seconds.")
+        await asyncio.sleep(e.value + 2)
+        logger.info("Floodwait ended")
